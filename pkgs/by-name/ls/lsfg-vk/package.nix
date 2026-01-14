c7907522f6f56fe2dfcf3c614ae73774e47853f2
Sarah Lament <sarah@lament.gay>
2026-01-14 01:09:55 -0600

    lsfg-vk: updated to 2.0.0

--- pkgs/by-name/ls/lsfg-vk/package.nix
+++ pkgs/by-name/ls/lsfg-vk/package.nix
@@ -4,33 +4,44 @@
   cmake,
   vulkan-headers,
   llvmPackages,
+  qt6
 }:
-
 llvmPackages.stdenv.mkDerivation rec {
   pname = "lsfg-vk";
-  version = "1.0.0";
+  version = "2.0.0-dev";
 
   src = fetchFromGitHub {
     owner = "PancakeTAS";
     repo = "lsfg-vk";
     tag = "v${version}";
-    hash = "sha256-hWpuPH7mKbeMaLaRUwtlkNLy4lOnJEe+yd54L7y2kV0=";
+    hash = "sha256-9f1epUbJNr8yaUfWNcVth88UfGP1kRX6+yOcA/60XL8=";
     fetchSubmodules = true;
   };
 
-  postPatch = ''
-    substituteInPlace VkLayer_LS_frame_generation.json \
-      --replace-fail "liblsfg-vk.so" "$out/lib/liblsfg-vk.so"
+  postInstall = ''
+    substituteInPlace $out/share/vulkan/implicit_layer.d/VkLayer_LSFGVK_frame_generation.json \
+      --replace-fail "liblsfg-vk-layer.so" "$out/lib/liblsfg-vk-layer.so"
   '';
 
   nativeBuildInputs = [
     llvmPackages.clang-tools
     llvmPackages.libllvm
+    qt6.qtbase
+    qt6.qtdeclarative
+    qt6.wrapQtAppsHook
     cmake
   ];
 
   buildInputs = [
     vulkan-headers
+  ];
+
+  cmakeFlags = [
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DLSFGVK_BUILD_VK_LAYER=On"
+    "-DLSFGVK_BUILD_CLI=On"
+    "-DLSFGVK_BUILD_UI=On"
+    "-DLSFGVK_INSTALL_XDG_FILES=On"
   ];
 
   meta = {
@@ -39,6 +50,9 @@ llvmPackages.stdenv.mkDerivation rec {
     changelog = "https://github.com/PancakeTAS/lsfg-vk/releases/tag/${src.tag}";
     license = lib.licenses.mit;
     platforms = lib.platforms.linux;
-    maintainers = with lib.maintainers; [ pabloaul ];
+    maintainers = with lib.maintainers; [
+      pabloaul
+      sarahlament
+    ];
   };
 }
